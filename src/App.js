892e03261be3f844942c44c7231a44eb8be4f804
Nirushetty10 <nirushetty582@gmail.com>
2023-08-02 18:02:47 +0530

    added routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import "./App.css"
 import Home from './Pages/Home';
-import Stats from './Pages/Stats';
-// import Cards from './Pages/Cards';
-import FooterPage from './Pages/FooterPage';
-import Subscribe from './Components/subscribe/Subscribe';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
-    <div>
-      <Home />
-      <Stats />
-      {/* <Cards /> */}
-      <Subscribe />
-      <FooterPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Home />}/>
+         <Route path='/about' element={<Home />}/>
+         <Route path='/contact' element={<Home />}/>
+         <Route path='/services' element={<Home />}/>
+         <Route path='/login' element={<Login />}/>
+         <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
+      
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Slider from '../Components/Slider/Slider'
+import Stats from '../Components/Stats/Stats'
+import Subscribe from '../Components/subscribe/Subscribe'
+import Footer from '../Components/Footer/Footer'
 
 const Home = () => {
   return (
     <div className='layout'>
       <Slider />
+      <Stats />
+      <Subscribe />
+      <Footer />
     </div>
   )
 }
