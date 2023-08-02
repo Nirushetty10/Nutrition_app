811115ec78fb59b5693b8c132e768c7d5b51aa5f
Nirushetty10 <nirushetty582@gmail.com>
2023-08-02 16:08:58 +0530

    added footer section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import "./App.css"
 import Home from './Pages/Home';
 import Stats from './Pages/Stats';
-import Cards from './Pages/Cards';
+// import Cards from './Pages/Cards';
+import FooterPage from './Pages/FooterPage';
 
 function App() {
   return (
     <div>
       <Home />
       <Stats />
-      <Cards />
+      {/* <Cards /> */}
+      <FooterPage />
     </div>
   )
 }
---
+++ src/Pages/FooterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../Components/Footer/Footer'
+
+const FooterPage = () => {
+  return (
+    <div>
+        <Footer />
+    </div>
+  )
+}
+
+export default FooterPage
