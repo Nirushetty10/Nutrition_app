977665cba5c5c39f071f90260b7af29f364406de
Nirushetty10 <nirushetty582@gmail.com>
2023-08-17 18:42:15 +0530

    responsiveness completed for home page and stats page

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import "./NavBar.scss"
 
 const NavBar = (props) => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className='nav_bar' style={{backgroundColor : `${props.type  === "auth" ? "black" : ""} `}}>
         <div className="logo">NutriDiet</div>
@@ -16,6 +19,16 @@ const NavBar = (props) => {
             <div className="link_section__button">
                 <button>Get Started</button>
             </div>
+            <div className='hamburger_menu' onClick={() => setMenuOpen(!menuOpen)}>
+                {menuOpen ? <CloseIcon /> : <MenuIcon />}
+            </div>
+            {menuOpen && <div className="hamburger_menu_item">
+            <ul>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Services</li>
+                </ul>
+            </div>}
         </div>}
     </div>
   )
