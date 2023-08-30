41ab6f4125a7ac381a31d3816b9a66a98612db10
Nirushetty10 <nirushetty582@gmail.com>
2023-08-30 21:50:04 +0530

    updating

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Stats from '../Components/Stats/Stats'
 import Subscribe from '../Components/subscribe/Subscribe'
 import Footer from '../Components/Footer/Footer'
 import Cards from '../Components/Card/Card'
-import ChooseUs from '../Components/CooseUs/ChooseUs'
+import ChooseUs from '../Components/ChooseUs/ChooseUs'
 import Reviews from '../Components/Reviews/Reviews'
 
 const Home = () => {
